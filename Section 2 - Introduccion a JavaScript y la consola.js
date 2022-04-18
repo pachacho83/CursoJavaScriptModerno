137c49bf50766df45211f12a9692469ac8e38b38
Pachacho83 <evalenzg@gmail.com>
2022-04-18 17:02:00 -0400

    Section 2

---
+++ Section 2 - Introduccion a JavaScript y la consola.js
@@ -0,0 +1,32 @@
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo =  c + d;
+
+// console.log('%c Mis variables', 'color:blue; font-weight: bold');
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+c = 'Hola de nuevo';
+
+// console.table({a, b, c, d, x });
+
+let miNuevoNombre = 'Fernando Herrera';
+
+alert('Hola Mundo');
+
+
+let nombre = prompt('¿Cuál es tu nombre?');
+console.log( nombre );
+console.log( '****' + nombre + '****' ); // ''
+
+const seleccion = confirm('¿Está seguro de borrar esto?');
+console.log( seleccion );
+
+// console.log( global );
+// alert('Hola desde app.js');
+// console.log( console.log('Hola Mundo') );
